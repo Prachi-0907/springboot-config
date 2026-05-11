7eff018fc80fec08d115dd3f929b24a275e023c4
“Prachi-QT” <prachi.ghatole@quore-tex.com>
2026-05-11 14:40:39 +0530

    added model class

---
+++ src/main/java/com/prachi/Git_demo/model/Student.java
@@ -0,0 +1,16 @@
+package com.prachi.Git_demo.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String surname;
+}
